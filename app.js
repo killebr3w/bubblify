b6d6b0f450d172489effd0399a7949a0fc1f910b
Nicholas Meyers <meyers.nicholas@gene.com>
2016-01-25 15:16:27 -0800

    getting ready for heroku

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ var users = require('./routes/users');
 
 var app = express();
 
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
